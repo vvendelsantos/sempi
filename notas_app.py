f164f44f846f80d9379924682bbf7d6bffc89124
vvendelsantos <producao.wendel@gmail.com>
2025-08-11 14:14:55 -0300

    notas_app.py

--- notas_app.py
+++ notas_app.py
@@ -917,8 +917,7 @@ def main():
     <p>Espero que esta mensagem os(as) encontre bem.</p>
 
     <p>
-      A Comissão Organizadora da <strong>VII Semana Acadêmica da Propriedade Intelectual (VII SEMPI)</strong> os(as) parabeniza pela apresentação do trabalho.
-      Abaixo, apresentamos as avaliações realizadas pelos membros do Comitê Científico, com base nos critérios previamente definidos:
+      A Comissão Organizadora da <strong>VII Semana Acadêmica da Propriedade Intelectual (VII SEMPI)</strong> apresenta os detalhes das avaliações referentes à apresentação oral, realizadas pelos membros do Comitê Científico com base nos critérios previamente estabelecidos.
     </p>
 
     <div class="box">
